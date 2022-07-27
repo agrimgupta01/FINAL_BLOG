6053fbd4c5149937115c8d3a7dc90c83bbad417a
agrimgupta01 <agrim921@gmail.com>
2022-07-27 06:11:41 +0530

    added README

--- final_blog/final_blog/settings.py
+++ final_blog/final_blog/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = 'django-insecure-l%idx+0(v)k$2!u+p^xgxo8*ie&p)gvsp*+06t%75u@i#fab!_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agrimivy-crm1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,12 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL='/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFIELS_DIRS = (os.path.join(BASE_DIR,'static'))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
